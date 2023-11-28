695b90ab4019474c56831d3ac036856fbe6f018a
MANUALPYTHON3 <152281719+MANUALPYTHON3@users.noreply.github.com>
2023-11-28 09:36:35 -0300

    commit

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Enteros
+a = 1
+b = 2
+c = 3
+
+# Flotantes
+d = 1.0
+e = 2.5
+f = 3.14
+
+# Cadenas
+g = "Hola, mundo!"
+h = "Python es un lenguaje de programación"
+i = "¡Muy fácil de aprender!"
+
+# Booleanos
+j = True
+k = False
